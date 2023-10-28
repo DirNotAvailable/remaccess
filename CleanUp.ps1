1138c9592c59432372756fca93bb3aca4de28403
沐宸 <62504289+DirNotAvailable@users.noreply.github.com>
2023-10-28 22:13:58 +0500

    Delete CleanUp.ps1

--- CleanUp.ps1
+++
@@ -1,7 +0,0 @@
-$psreadlineFolderPath = Join-Path $env:USERPROFILE 'AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine'
-if (Test-Path -Path $psreadlineFolderPath -PathType Container) {
-    $files = Get-ChildItem -Path $psreadlineFolderPath
-    if ($files.Count -gt 0) {
-        Remove-Item -Path "$psreadlineFolderPath\*" -Force
-    }
-}
